970d47ff001e7acb94e15bd6f9aa3055c23256d9
milesba4 <milesbaffour@gmail.com>
2022-07-14 17:41:10 -0400

    rendering route components

--- capstone-project/ui/src/components/App/App.jsx
+++ capstone-project/ui/src/components/App/App.jsx
@@ -1,16 +1,24 @@
-import * as React from "react"
-
-import './App.css'
+import * as React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
 
 export default function App() {
-  
   return (
     <div className="App">
       <React.Fragment>
-    
+        {
+          <BrowserRouter>
+            <main>
+              <Routes>
+
+
+
+                
+              </Routes>
+            </main>
+          </BrowserRouter>
+        }
       </React.Fragment>
     </div>
-  )
+  );
 }
-
-
