8b74a4bb628c362c22c4e7a4f2271841c4e2cc41
milesba4 <milesbaffour@gmail.com>
2022-07-18 13:57:33 -0400

    rendering text inside product card component

--- capstone-project/ui/src/components/ContactCard/ContactCard.jsx
+++ capstone-project/ui/src/components/ContactCard/ContactCard.jsx
@@ -6,8 +6,29 @@ import "./ContactCard.css";
 
 export default function ContactCard({}) {
     return (
+    <div className = "contact-card">
+        <ul className = "info">
+            <li>
+            <span className = "label">Email:</span>
+            <span></span>
+            </li>
+            <li>    
+            <span className = "label">Phone:</span>
+            <span></span>    
+            </li>
+            <li>   
+            <span className = "label">Address:</span>
+            <span></span>   
+            </li>
+            <li>    
+            <span className = "label">Socials:</span>
+            <span></span>    
+            </li>
 
 
 
 
+
+        </ul>
+    </div>    
     )}
--- capstone-project/ui/src/components/ContactUs/ContactUs.jsx
+++ capstone-project/ui/src/components/ContactUs/ContactUs.jsx
@@ -7,6 +7,7 @@ export default function ContactUs({}) {
   return (
     <div className="contact-us">
       <div className="content">
+        <h1>Contact Us</h1>
         <div className="contactus-card">
         <ContactCard/>
         </div>
