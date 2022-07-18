7b0eeb8e69f192d3111e6e3638bfd4104acaf5bf
milesba4 <milesbaffour@gmail.com>
2022-07-18 12:14:26 -0400

    rendering intro text in landingPage component

--- capstone-project/ui/src/components/LandingPage/LandingPage.jsx
+++ capstone-project/ui/src/components/LandingPage/LandingPage.jsx
@@ -1,14 +1,13 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
-import "./LandingPage.css"
+import "./LandingPage.css";
 
 export default function LandingPage({}) {
-    return (
-    <div className = "landing-page">
-
-    
-
-
-
+  return (
+    <div className="landing-page">
+      <div className="intro">
+        <h1>Welcome!</h1>
+      </div>
     </div>
-    )}
+  );
+}
