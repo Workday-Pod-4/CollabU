b8003822e4dd34ab474e86a6aded2288ea40697d
Godmiliut <supermiliut72@gmail.com>
2022-07-18 12:40:07 -0400

    LoginPage renders a footer that links to the RegistrationPage.jsx

---
+++ capstone-project/ui/src/components/LoginPage/LoginPage.jsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import LoginForm from "./LoginForm"
+import "./LoginPage.css"
+
+export default function LoginPage() {
+  return (
+    <div className="login-page">
+      <h2>Login</h2>
+      <LoginForm />
+      <div className="footer">
+        <p>
+          Don't have an account? Sign up
+          <Link to="/register"> here.</Link>
+        </p>
+        </div>
+    </div>
+  )
+}
