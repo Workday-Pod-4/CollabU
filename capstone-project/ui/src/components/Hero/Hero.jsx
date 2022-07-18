49ed8a7c92bf6885d35f7b207b30244ce27aade7
milesba4 <milesbaffour@gmail.com>
2022-07-18 12:18:47 -0400

    creating hero component and importing it in LandingPage component

---
+++ capstone-project/ui/src/components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+
+
+
+export default function Hero(){
+
+return(
+
+
+
+
+
+
+
+
+
+
+)
+}
--- capstone-project/ui/src/components/LandingPage/LandingPage.jsx
+++ capstone-project/ui/src/components/LandingPage/LandingPage.jsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
 import "./LandingPage.css";
+import Hero from "../Hero/Hero";
 
 export default function LandingPage({}) {
   return (
