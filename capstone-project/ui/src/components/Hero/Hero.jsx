95ddacc01abdde950faead7589540bd168de3b1d
milesba4 <milesbaffour@gmail.com>
2022-07-18 13:12:49 -0400

    centering intro and adding placeholder text

--- capstone-project/ui/src/components/Hero/Hero.jsx
+++ capstone-project/ui/src/components/Hero/Hero.jsx
@@ -6,10 +6,11 @@ export default function Hero({}) {
     <div className="hero">
       <div className="content">
         <div className="hero-img">
-          <img src="" alt="placeholder"></img>
+          <img src="" alt="placeholder" ></img>
         </div>
         <div className="intro">
           <h1>Welcome!</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta magni quis numquam minima culpa incidunt, quas, vitae voluptatum dolorem fuga inventore porro fugit at corporis, ad autem. Quas dolor totam beatae voluptatibus iusto libero ullam, ea maiores, aliquid veritatis, assumenda non doloribus vel iure molestiae.</p>
         </div>
       </div>
     </div>
