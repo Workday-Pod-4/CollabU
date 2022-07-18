df11d67a1d1c494936ca02eae9cf61655b83d694
Godmiliut <supermiliut72@gmail.com>
2022-07-18 13:27:59 -0400

    Saving components import for later after merge

--- capstone-project/ui/src/components/App/App.jsx
+++ capstone-project/ui/src/components/App/App.jsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "../Navbar/Navbar";
-import RegistrationPage from "../RegistrationPage/RegistrationPage";
 
 export default function App() {
   return (
@@ -13,7 +12,7 @@ export default function App() {
           <Navbar/>
             <main>
               <Routes>
-                <Route path="/register" element={<RegistrationPage />} />
+                
 
 
 
