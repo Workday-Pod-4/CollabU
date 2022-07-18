07580da10fe498524b22835e551b5e74307340bf
milesba4 <milesbaffour@gmail.com>
2022-07-18 13:05:38 -0400

    styling about section and adding placeholder text

--- capstone-project/ui/src/components/About/About.jsx
+++ capstone-project/ui/src/components/About/About.jsx
@@ -7,7 +7,7 @@ export default function About({}) {
     <div className="about">
       <div className="content">
         <h1>About</h1>
-
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam corporis cumque, error consequatur beatae sequi pariatur nesciunt officiis voluptates doloribus asperiores sit praesentium ipsam non.</p>
       </div>
     </div>
   );
--- capstone-project/ui/src/components/LandingPage/LandingPage.jsx
+++ capstone-project/ui/src/components/LandingPage/LandingPage.jsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { Link } from "react-router-dom";
 import "./LandingPage.css";
 import Hero from "../Hero/Hero";
-import AboutUs from "../About/About";
+import About from "../About/About";
 
 export default function LandingPage({}) {
   return (
