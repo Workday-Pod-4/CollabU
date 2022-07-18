8143b2dc095937967a1558f1202447f7bb053bf2
milesba4 <milesbaffour@gmail.com>
2022-07-18 12:59:28 -0400

    creating AboutUs component and importing it into LandingPage component

---
+++ capstone-project/ui/src/components/AboutUs/AboutUs.jsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+import "./AboutUs.css";
+
+export default function AboutUs({}) {
+  return (
+    <div className="about-us">
+      <div className="content">
+
+        
+      </div>
+    </div>
+  );
+}
--- capstone-project/ui/src/components/LandingPage/LandingPage.jsx
+++ capstone-project/ui/src/components/LandingPage/LandingPage.jsx
@@ -2,12 +2,16 @@ import * as React from "react";
 import { Link } from "react-router-dom";
 import "./LandingPage.css";
 import Hero from "../Hero/Hero";
+import AboutUs from "../AboutUs/AboutUs";
 
 export default function LandingPage({}) {
   return (
     <div className="landing-page">
       <div className="hero-banner">
-        <Hero/>
+        <Hero />
+      </div>
+      <div className="about-us-section">
+        <AboutUs />
       </div>
     </div>
   );
