d2677ba2fefa1ed52ee751f55b3b71751db24619
milesba4 <milesbaffour@gmail.com>
2022-07-19 23:31:00 -0400

    sectioning profile page

--- capstone-project/ui/src/components/ProfilePage/ProfilePage.jsx
+++ capstone-project/ui/src/components/ProfilePage/ProfilePage.jsx
@@ -4,25 +4,41 @@ import "./ProfilePage.css";
 import apiClient from "../../services/apiClient";
 import { useAuthContext } from "../../contexts/auth";
 
-export default function ProfilePage(){
-    const {user} = useAuthContext()
+export default function ProfilePage() {
+  const { user } = useAuthContext();
 
+  React.useEffect(() => {
+    console.log("userInfo:", user);
+  }, [user]);
 
-    React.useEffect(() => {
-        console.log("userInfo:", user)
+  return (
+    <div className="profile-page">
+      <h1>Profile</h1>
+      <div className="sections">
+        <div className="left-section">
+          <h1>left-section</h1>
+          <div className="profile-pic">
+            <p>Profile img</p>
+          </div>
+          <div className = "profile-name">
+            <h2>{user.first_name}  {user.last_name}</h2>
+          </div>
+        </div>
 
-    }, [user])
+        <div className="middle-section">
+          <h1> middle-section</h1>
+          <div className="info-cards">
+            <p>middle</p>
+          </div>
+        </div>
 
-
-
-
-return (
-
-<h1>Profile</h1>
-
-
-
-
-
-)
+        <div className="right-section">
+          <h1>right-section</h1>
+          <div className="match-history">
+            <p>right</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
