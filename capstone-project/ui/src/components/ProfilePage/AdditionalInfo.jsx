1370a669f1b683b0c48cae74cb717f50c9a183bd
Godmiliut <supermiliut72@gmail.com>
2022-07-21 17:01:07 -0400

    Finished formatting the AdditionalInfo.jsx component

--- capstone-project/ui/src/components/ProfilePage/AdditionalInfo.jsx
+++ capstone-project/ui/src/components/ProfilePage/AdditionalInfo.jsx
@@ -5,7 +5,7 @@ import "./AdditionalInfo.css"
 
 export default function AdditionalInfo() {
 
-  const { user, setUser } = useAuthContext()
+  const { user, setUser, setFirstTime } = useAuthContext()
 
   const [isProcessing, setIsProcessing] = React.useState(false)
   const [error, setError] = React.useState("")
@@ -21,7 +21,7 @@ export default function AdditionalInfo() {
 
      setIsProcessing(true);
 
-    const { data, error } = await apiClient.updateUserInfo({  
+    /*const { data, error } = await apiClient.updateUserInfo({  
         college: form.college, 
         major: form.major, 
         workTitle: form.workTitle, 
@@ -31,68 +31,85 @@ export default function AdditionalInfo() {
         socialMediaLink2: form.socialMediaLink2,
         socialMediaLink3: form.socialMediaLink3
      })
-    if(error) setError("Error trying to update your information")
+    if(error) {
+        setError("Error trying to update your information")
+        console.log("Not reaching here")
+    }
     
     if(data?.user) {
       setUser(data.user)
-    }
+      setFirstTime(false)
+      console.log("Hello!! You made it")
+    }*/
+
+    setFirstTime(false)
+    
+    console.log("Hello!! You made it")
 
     setIsProcessing(false)
   }
 
     return (
-      <div className="additional-form">
-        <div className="input-field">
-            <label>College</label>
-            <input className="form-input" type="text" name="username" placeholder="Username" onChange={handleOnChange} defaultValue={form.username}></input>
+        <div className="container" id="container">
+            <div className="form-wrapper">
+                <div className="header">
+                    <h2>Additional Information</h2>
+                </div>
+                <div className="additional-form">
+                    <div className="input-field">
+                        <label>College</label>
+                        <select className="form-input" name="college" placeholder="College" onChange={handleOnChange} defaultValue={form.college}>
+                            <option value={"college1"}>College 1</option>
+                            <option value={"college2"}>College 2</option>
+                            <option value={"college3"}>College 3</option>
+                        </select>
+                    </div>
+                    <div className="input-field">
+                        <label>Major</label>
+                        <select className="form-input" name="major" placeholder="Major" onChange={handleOnChange} defaultValue={form.major}>
+                            <option value={"major1"}>Major 1</option>
+                            <option value={"major2"}>Major 2</option>
+                            <option value={"major3"}>Major 3</option>
+                        </select>
+                    </div>
+                    <div className="input-field">
+                        <label>Work Title</label>
+                        <select className="form-input" name="workTitle" placeholder="Work Title" onChange={handleOnChange} defaultValue={form.workTitle}>
+                            <option value={"worktitle1"}>Work Title 1</option>
+                            <option value={"worktitle2"}>Work Title 2</option>
+                            <option value={"worktitle3"}>Work Title 3</option>
+                        </select>
+                    </div>
+                    <div className="input-field">
+                        <label>Industry</label>
+                        <select className="form-input" name="industry" placeholder="Industry" onChange={handleOnChange} defaultValue={form.industry}>
+                            <option value={"industry1"}>Industry 1</option>
+                            <option value={"industry2"}>Industry 2</option>
+                            <option value={"industry3"}>Industry 3</option>
+                        </select>
+                    </div>
+                    <div className="input-field">
+                        <label>Years of Experience</label>
+                        <select className="form-input" name="yearsOfExperience" placeholder="Years of Experience" onChange={handleOnChange} defaultValue={form.yearsOfExperience}>
+                            <option value={"0-2"}>0 - 2</option>
+                            <option value={"3-9"}>3 - 9</option>
+                            <option value={"10+"}>10+</option>
+                        </select>
+                    </div>
+                    <div className="input-field">
+                        <label>Social Links</label>
+                        <input className="form-input" type="text" name="socialMediaLink1" placeholder="Social Link 1" onChange={handleOnChange} defaultValue={form.socialMediaLink1}></input>
+                    </div>
+                    <div className="input-field">
+                        <input className="form-input" type="text" name="socialMediaLink2" placeholder="Social Link 2" onChange={handleOnChange} defaultValue={form.socialMediaLink2}></input>
+                    </div>
+                    <div className="input-field">
+                        <input className="form-input" type="text" name="socialMediaLink3" placeholder="Social Link 3" onChange={handleOnChange} defaultValue={form.socialMediaLink3}></input>
+                    </div>
+                    <button className="btn" onClick={saveInfo}>Save</button>
+                    <p className="close" onClick={saveInfo}>Skip for now!</p>
+                </div>
+            </div>
         </div>
-        <div className="input-field">
-            <label>College</label>
-            <select className="form-input" name="college" placeholder="College" onChange={handleOnChange} defaultValue={form.college}>
-                <option value={"college1"}>College 1</option>
-                <option value={"college2"}>Location 2</option>
-                <option value={"college3"}>Location 3</option>
-            </select>
-        </div>
-        <div className="input-field">
-            <label>Major</label>
-            <select className="form-input" name="major" placeholder="Major" onChange={handleOnChange} defaultValue={form.major}>
-                <option value={"major1"}>Major 1</option>
-                <option value={"major2"}>Major 2</option>
-                <option value={"major3"}>Major 3</option>
-            </select>
-        </div>
-        <div className="input-field">
-            <label>Work Title</label>
-            <select className="form-input" name="workTitle" placeholder="Work Title" onChange={handleOnChange} defaultValue={form.workTitle}>
-                <option value={"worktitle1"}>Work Title 1</option>
-                <option value={"worktitle2"}>Work Title 2</option>
-                <option value={"worktitle3"}>Work Title 3</option>
-            </select>
-        </div>
-        <div className="input-field">
-            <label>Industry</label>
-            <select className="form-input" name="industry" placeholder="Industry" onChange={handleOnChange} defaultValue={form.industry}>
-                <option value={"industry1"}>Industry 1</option>
-                <option value={"industry2"}>Industry 2</option>
-                <option value={"industry3"}>Industry 3</option>
-            </select>
-        </div>
-        <div className="input-field">
-            <label>Years of Experience</label>
-            <select className="form-input" name="yearsOfExperience" placeholder="Years of Experience" onChange={handleOnChange} defaultValue={form.yearsOfExperience}>
-                <option value={"0-2"}>0 - 2</option>
-                <option value={"3-9"}>3 - 9</option>
-                <option value={"10+"}>10+</option>
-            </select>
-        </div>
-        <div className="input-field">
-            <label>Social Links</label>
-            <input className="form-input" type="text" name="socialMediaLink1" placeholder="Social Link 1" onChange={handleOnChange} defaultValue={form.socialMediaLink1}></input>
-            <input className="form-input" type="text" name="socialMediaLink2" placeholder="Social Link 2" onChange={handleOnChange} defaultValue={form.socialMediaLink2}></input>
-            <input className="form-input" type="text" name="socialMediaLink3" placeholder="Social Link 3" onChange={handleOnChange} defaultValue={form.socialMediaLink3}></input>
-        </div>
-        <button className="btn" onClick={saveInfo}>Update</button>
-      </div>
     )
   }
