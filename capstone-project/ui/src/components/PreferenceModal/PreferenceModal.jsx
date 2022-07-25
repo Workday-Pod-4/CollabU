67c53eb851172549df7881dd3dd1e8739516e138
milesba4 <milesbaffour@gmail.com>
2022-07-25 13:17:00 -0400

    replacing input fields with dropdowns and styling preference modal

--- capstone-project/ui/src/components/PreferenceModal/PreferenceModal.jsx
+++ capstone-project/ui/src/components/PreferenceModal/PreferenceModal.jsx
@@ -32,48 +32,41 @@ export default function PreferenceModal() {
             <div className="header">
               <h1>Are you studying or working?</h1>
             </div>
-            <button className="close-modal" onClick={togglePrefModal}> 
+            <li className="close-modal" onClick={togglePrefModal}> 
             x
-            </button>
+            </li>
             <div className="form-wrapper">
               {isStudying?<div className="preference-study-form">
               <div className ="sub-header"> Study </div>
                 <ul>
-                  <li>
-                    <input
-                      className="form-input"
-                      type="text"
-                      name="subject"
-                      placeholder="Subject/Course"
-                      value=""
-                    ></input>
-                  </li>
-                  <li>
+                <li>
                     <span>
-                      <input
-                        className="form-input"
-                        type="text"
-                        name="topic"
-                        placeholder="Topic Area"
-                        value=""
-                      ></input>
+                    <label>Topic</label>
+                    <select className="form-input" name="topic" placeholder="Select topic" >
+                        <option value={"Mathematics"}>Mathematics</option>
+                        <option value={"Computer Science"}>Electrical Engineering</option>
+                        <option value={"Biology"}>Biology</option>
+                        <option value={"Chemistry"}>Chemistry</option>
+                    </select>
+                    </span>
+                  </li>
+                <li>
+                  <span>
+                    <label>Subject/Course</label>
+                    <br></br>
+                    <select className="form-input" name="subject" placeholder="Select Subject" >
+                        <option value={"Intro to programmings"}>Intro to programming</option>
+                        <option value={"Calculus 1"}>Calculus 1</option>
+                        <option value={"Intro to Biology"}>Intro to biology</option>
+                        <option value={"Organic Chemistry"}>Organic chemistry</option>
+                    </select>
                     </span>
                   </li>
                 </ul>
               </div>:(isWorking?<div className="preference-work-form">
                 <div className ="sub-header"> Work </div>
                 <ul>
-                  <li>
-                    <span>
-                    <label>Type of Work</label>
-                    <select className="form-input" name="type-of-work" placeholder="Select type of work" >
-                        <option value={"Software-Developement"}>Software Development</option>
-                        <option value={"Electrical-Engineering"}>Electrical Engineering</option>
-                        <option value={"Consulting"}>Financial Consulting</option>
-                    </select>
-                    </span>
-                  </li>
-                  <li>
+                <li>
                   <span>
                     <label>Industry</label>
                     <br></br>
@@ -81,6 +74,16 @@ export default function PreferenceModal() {
                         <option value={"Technology"}>Technology</option>
                         <option value={"Medicine"}>Medicine</option>
                         <option value={"Consulting"}>Consulting</option>
+                    </select>
+                    </span>
+                  </li>
+                  <li>
+                    <span>
+                    <label>Type of Work</label>
+                    <select className="form-input" name="type-of-work" placeholder="Select type of work" >
+                        <option value={"Software-Developement"}>Software Development</option>
+                        <option value={"Electrical-Engineering"}>Electrical Engineering</option>
+                        <option value={"Consulting"}>Financial Consulting</option>
                     </select>
                     </span>
                   </li>
