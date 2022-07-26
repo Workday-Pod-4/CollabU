e27e14578a50c857446f8a9dc20cafd9422a2df3
Godmiliut <supermiliut72@gmail.com>
2022-07-26 11:48:41 -0400

    Changes to Chatbox

--- capstone-project/ui/src/components/ChatRoom/ChatRoom.jsx
+++ capstone-project/ui/src/components/ChatRoom/ChatRoom.jsx
@@ -80,29 +80,5 @@ export default function ChatRoom() {
                 </div>
             </div>
             }
-            {/* <div className="user-views">
-                <div className="user-view">
-                    <h1>Name</h1>
-                    <div className="user-video">
-                     <h1>User 1 Video</h1>
-                    </div>
-                </div>
-                <div className="user-view">
-                    <h1>Name</h1>
-                    <div className="user-video">
-                        <h1>User 2 Video</h1>
-                    </div>
-                </div>
-            </div>
-            <div className="bottom-row">
-                <div className="button-container">
-                    <button className="mute">Mute</button>
-                    <button className="video">Video</button>
-                </div>
-                <div className={cName}>
-                    <button className={bName} onClick={() => (setChatOpen(!chatOpen))}>Chat</button>
-                </div>
-            </div>
-        </div> */}
     </div>    
     )}
