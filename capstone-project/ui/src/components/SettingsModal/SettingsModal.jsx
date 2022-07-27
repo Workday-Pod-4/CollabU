fc5773b498140f2e451f5ab248cb12e61ef45303
milesba4 <milesbaffour@gmail.com>
2022-07-27 13:00:36 -0400

    connecting settings modal to backend

--- capstone-project/ui/src/components/SettingsModal/SettingsModal.jsx
+++ capstone-project/ui/src/components/SettingsModal/SettingsModal.jsx
@@ -1,18 +1,45 @@
 import * as React from "react";
 import "./SettingsModal.css";
 import { useAuthContext } from "../../contexts/auth";
-
+import apiClient from "../../services/apiClient";
 
 export default function SettingsModal() {
-    const {toggleSettingsModal,settingsModal} = useAuthContext();
-    // function handleOnChange(evt){
+  const {
+    toggleSettingsModal,
+    settingsModal,
+    setSettingsModal,
+    user,
+    error,
+    setError,
+  } = useAuthContext();
+  const [form, setForm] = React.useState({
+    email: "",
+    username: "",
+    password: "",
+    confirmNewPassword: "",
+    newPassword: "",
+  });
 
+  function handleOnChange(evt) {
+    setForm((f) => ({ ...f, [evt.target.name]: evt.target.value }));
+  }
 
-    // }
-    
-    // function resetPassword(){
-    
-    // }
+  async function resetPassword(evt) {
+    evt.preventDefault();
+    const { data, error } = await apiClient.updateUserPassword({
+      email: user.email,
+      username: user.username,
+      password: form.password,
+      confirmNewPassword: form.confirmNewPassword,
+      newPassword: form.newPassword,
+    });
+    //closing settings modal if password is successfully changed
+    setSettingsModal(!settingsModal);
+    if (error) {
+      setSettingsModal(settingsModal);
+      setError(error);
+    }
+  }
 
   return (
     <div className="settings-modal-container">
@@ -20,8 +47,8 @@ export default function SettingsModal() {
         <div className="settings-header">
           <h1>Settings</h1>
         </div>
-        <li className="close-settings-modal" onClick={toggleSettingsModal} > 
-            x
+        <li className="close-settings-modal" onClick={toggleSettingsModal}>
+          x
         </li>
         <div className="settings-form-wrapper">
           <div className="password-reset-form">
@@ -29,23 +56,58 @@ export default function SettingsModal() {
               <li>
                 <span>
                   <label>Current Password</label>
-                  <input className="form-input" type="password" name="current-password-reset" placeholder="Current Password" defaultValue="" ></input>
+                  <input
+                    className="form-input"
+                    type="password"
+                    name="password"
+                    placeholder="Current Password"
+                    defaultValue={form.password}
+                    onChange={handleOnChange}
+                  ></input>
+
+                  {error != "Password does not match." ? (
+                    <p className="error">{error}</p>
+                  ) : null}
                 </span>
               </li>
               <li>
                 <span>
                   <label>New Password</label>
-                  <input className="form-input" type="password" name="new-password-reset" placeholder="New Password" defaultValue=""></input>
+                  <input
+                    className="form-input"
+                    type="password"
+                    name="newPassword"
+                    placeholder="New Password"
+                    defaultValue={form.newPassword}
+                    onChange={handleOnChange}
+                  ></input>
+
+                  {error != "Invalid email/password" ? (
+                    <p className="error">{error}</p>
+                  ) : null}
                 </span>
               </li>
               <li>
                 <span>
                   <label>Confirm New Password</label>
-                  <input className="form-input" type="password" name="confirm-new-password-reset" placeholder="New Password" defaultValue=""></input>
+                  <input
+                    className="form-input"
+                    type="password"
+                    name="confirmNewPassword"
+                    placeholder="New Password"
+                    defaultValue={form.confirmNewPassword}
+                    onChange={handleOnChange}
+                  ></input>
+
+                  {error != "Invalid email/password" ? (
+                    <p className="error">{error}</p>
+                  ) : null}
                 </span>
               </li>
             </ul>
-            <button className="settings-update-btn">Update</button>
+            <button className="settings-update-btn" onClick={resetPassword}>
+              Update
+            </button>
           </div>
         </div>
       </div>
