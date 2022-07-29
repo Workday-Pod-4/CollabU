5ab9c80519cd4f7e2ff7cc9c2b8de6fefeb9a8e3
Godmiliut <supermiliut72@gmail.com>
2022-07-29 14:37:18 -0400

    Started to implement a button to get out of the queue

--- capstone-project/ui/src/components/Loading/Loading.jsx
+++ capstone-project/ui/src/components/Loading/Loading.jsx
@@ -1,11 +1,20 @@
 import * as React from "react";
+import { useAuthContext } from "../../contexts/auth";
 import "./Loading.css";
 
 
 export default function Loading() {
+
+  const { setIsLoading } = useAuthContext();
+
   return (
     <div className="container">
         <div className="content">
+          <div className="header">
+            <div className="button-container">
+              <button className="close" onClick={() => {setIsLoading(false)}}> x </button>
+            </div>
+          </div>
           <h1>Finding you a Match...</h1>
           <p> If it's taking a while to find a match, it's possible there's no one online. </p>
           <p> If that's the case, please try again later. </p>
