2d9a87020de155c8cd28a53e7e59e2357fbffc0e
Godmiliut <supermiliut72@gmail.com>
2022-08-07 02:16:46 -0400

    Removed email field from report issue modal

--- capstone-project/ui/src/components/ReportIssueModal/ReportIssueModal 2.jsx
+++ capstone-project/ui/src/components/ReportIssueModal/ReportIssueModal 2.jsx
@@ -5,8 +5,10 @@ import "./ReportIssueModal.css";
 
 
 export default function ReportIssueModal() {
+
+  const { user } = useAuthContext();
   const [state, handleSubmit] = useForm("xyyorrjy");
-  const [form, setForm] = React.useState({ "message": ""})
+  const [form, setForm] = React.useState({ "email": `${user.email}`, "message": ""})
   const {toggleReportModal } = useAuthContext();
   function handleOnChange(evt){
     setForm((f) => ({...f, [evt.target.name]: evt.target.value}))
@@ -25,13 +27,6 @@ export default function ReportIssueModal() {
             <div className = "report-issue-form">
             <div className = "content">
             <form onSubmit={handleSubmit}>
-              <div className="email-input" >
-                <label htmlFor="email"/>
-                  <input className="form-input"  type="text" name="email" placeholder="Email" onChange={handleOnChange} defaultValue={form.firstName}></input>
-                  
-                  <p className = "validation-error"> <ValidationError prefix="Email" field="email" errors={state.errors}/></p>
-              </div>
-
             <div className="message-input">
               <textarea id ="message" name="message" className="form-input" type="text" placeholder="Leave your message here..." onChange={handleOnChange} value={form.message} />
               <ValidationError prefix="Message" field="message"errors={state.errors}/>
